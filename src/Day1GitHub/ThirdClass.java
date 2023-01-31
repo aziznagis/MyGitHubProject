107cb2d96899ead21be5ef179b1549d27cd8e221
azizh <azizhnagis@gmail.com>
2023-01-30 21:46:31 -0500

    I added a new class named thirdGitHub.

---
+++ src/Day1GitHub/ThirdClass.java
@@ -0,0 +1,14 @@
+package Day1GitHub;
+
+public class ThirdClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am writing this on aziznagisFT");
+
+
+    }
+
+
+
+}
