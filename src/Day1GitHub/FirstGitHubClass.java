25475b8a2a9ba9001c30c227407dc4ae2724096f
azizh <azizhnagis@gmail.com>
2023-01-30 20:25:07 -0500

    on line 17 runtime exception was corrected.

---
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -0,0 +1,13 @@
+package Day1GitHub;
+
+public class FirstGitHubClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java");
+        System.out.println(1+1);
+
+    }
+
+
+}
---
+++ src/Day1GitHub/Second.java
@@ -0,0 +1,11 @@
+package Day1GitHub;
+
+public class Second {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello second class for GIT");
+
+
+    }
+}
