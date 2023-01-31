7d24a31b79da66c323d074f3c67ad10f36d13a78
aziznagis <123660336+aziznagis@users.noreply.github.com>
2023-01-30 21:07:57 -0500

    Update FirstGitHubClass.java
    java was removed on line 7

--- src/Day1GitHub/FirstGitHubClass.java
+++ src/Day1GitHub/FirstGitHubClass.java
@@ -4,7 +4,7 @@ public class FirstGitHubClass {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello Java");
+        System.out.println("Hello");
         System.out.println(1+1);
 
     }
